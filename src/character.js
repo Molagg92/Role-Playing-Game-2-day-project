c17d9f562519da74fd1cdddbfc7852bc68f4b66f
Erik Z <molagg92@gmail.com>
2023-04-12 15:43:39 -0700

    fill out he attak and kill meathods for when our player defeats a monster, and gets stronger
    Co-authored by Brandon Morgan <brandmorgan@gmail.com>, and Hernan Verar <hernanverar@gmail.com>

--- src/character.js
+++ src/character.js
@@ -1,15 +1,22 @@
 
-
 export default class Character {
-  constructor(name, healthPoints, power) {
+  constructor(name, healthPoints, power, currXp) {
     this.name = name;
     this.healthPoints = healthPoints;
     this.power = power;
+    this.currXp = currXp;
+
     
   }
   attack(target) {
     target.healthPoints -= this.power;
   console.log(`${this.name} attacks ${target.name} for ${this.power} damage`);
   }
+  kill (target) {
+    if (target.healthPoints <= 0){
+      return character1.currXp += target.giveXp;
+    }
+  }
 }
+
 
--- src/monsters.js
+++ src/monsters.js
@@ -1,11 +1,11 @@
 import Character from "./character";
 
 export default class Monsters {
-  constructor(name, healthPoints, power) {
+  constructor(name, healthPoints, power, giveXp) {
     this.name = name;
     this.healthPoints = healthPoints;
     this.power = power;
-    
+    this.giveXp = giveXp;
   }
 
 attack(target) {
